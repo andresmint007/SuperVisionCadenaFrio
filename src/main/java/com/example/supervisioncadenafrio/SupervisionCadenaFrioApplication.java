9183043032cc1ebc1d83c3ae485e99aac6d80406
andresmint007 <andreslab7@gmail.com>
2025-09-06 10:46:09 -0500

    commit inicial

---
+++ src/main/java/com/example/supervisioncadenafrio/SupervisionCadenaFrioApplication.java
@@ -0,0 +1,13 @@
+package com.example.supervisioncadenafrio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisionCadenaFrioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupervisionCadenaFrioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/supervisioncadenafrio/SupervisionCadenaFrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supervisioncadenafrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisionCadenaFrioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
